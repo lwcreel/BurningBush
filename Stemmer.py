2a4b044555bc7a5d6cad1f71df4fa4a7e2a5e3a4
lwcreel <landon.creel@icloud.com>
2020-03-24 08:49:56 -0500

    stopword removal

--- Stemmer.py
+++ Stemmer.py
@@ -1,5 +1,7 @@
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+#from gensim.summarization.bm25 import get_bm25_weights
 
 # create stemmer object to simplify
 stemmer = PorterStemmer()
@@ -15,14 +17,20 @@ quranOut = open("Docs/StemmedQuran.txt", "w")
 # read data and split into words
 bibleText = bible.read()
 bibleText = bibleText.split(" ")
+bibleText = [word for word in bibleText if word not in stopwords.words('english')]
 quranText = quran.read()
 quranText = quranText.split(" ")
+quranText = [word for word in quranText if word not in stopwords.words('english')]
 
 # iterate through the lists, simplifying words and writing to output
-for i in bibleText:
+for i in bibleText:    
     bibleOut.write(stemmer.stem(i))
     bibleOut.write(" ")
 
 for i in quranText:
     quranOut.write(stemmer.stem(i))
     quranOut.write(" ")
+
+bibleText = bibleOut.read().split(" ")
+quranText = quran.read().split(" ")
+corpus = [bibleText, quranText]
